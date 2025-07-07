be931e617509d7fe00043448bd1581b1f76e636c
devjeverson <developer.oliveirajv@gmail.com>
2025-07-07 00:44:33 -0300

    Add email configuration and update test class name

---
+++ src/main/java/com/emails/contatoemail/config/EmailConfig.java
@@ -0,0 +1,27 @@
+package com.emails.contatoemail.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(System.getenv("EMAIL_USERNAME"));
+        mailSender.setPassword(System.getenv("EMAIL_PASSWORD"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
